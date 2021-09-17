5916285e73e3315dc971f36d117bc2077c47cb23
Ivan Mamenemuno <ivan.mamenemuno@mindfoundry.ai>
2021-09-17 21:59:49 +0100

    06 added click event listener to each light toggling active status

--- controller.js
+++ controller.js
@@ -3,8 +3,24 @@ const red = getElement( 'red')
 const amber = getElement( 'amber')
 const green = getElement( 'green')
  
-const lights = {red, amber, green}
+const lights = [red, amber, green] 
 
+lights.forEach( light => light.addEventListener('click', function(){
+    clearActive(lights) // clear active from all lights
+
+    // if light clicked doesnt have active class, add it else ignore
+    if ( !light.classList.contains('active') ) light.classList.add('active')
+
+}))
+
+// select elements from html file
 function getElement( id ){
     return document.getElementById( id )
+}
+
+function clearActive(lights) {
+    // loop over each light
+    lights.forEach(light => {
+        light.classList.remove('active')
+    });
 }
